e9d71ebb137a3743e384c576336d55e984ca4f0b
Scott Matthewman <scott.matthewman@chapterone.org>
2025-02-07 18:17:39 +0000

    client: Add vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,5 +14,8 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
+  },
+  test: {
+    environment: 'jsdom',
   }
 })
