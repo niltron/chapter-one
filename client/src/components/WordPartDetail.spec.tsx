35f6b736e10edd4da28dd60dc071c2b7774e9289
niltron <51491500+niltron@users.noreply.github.com>
2025-09-28 17:56:27 +0000

    Update Unit Test click event

--- client/src/components/WordPartDetail.spec.tsx
+++ client/src/components/WordPartDetail.spec.tsx
@@ -1,18 +1,18 @@
-import {describe, expect, it, vi} from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import WordPartDetail from "./WordPartDetail.tsx";
-import {fireEvent, render, screen} from "@testing-library/react";
-import {WordPart} from "../types/WordPart.ts";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { WordPart } from "../types/WordPart.ts";
 
 describe("WordPartDetail", () => {
     it("calls the onNeedsWork callback when the Needs Work button is clicked", () => {
         const onNeedsWork = vi.fn();
         const onMastered = vi.fn();
 
-        const wordPart: WordPart = {id: 1, label: "test"};
+        const wordPart: WordPart = { id: 1, label: "test", phonics_level_id: 1 };
 
-        render(<WordPartDetail wordPart={wordPart} onMastered={onMastered} onNeedsWork={onNeedsWork}/>);
+        render(<WordPartDetail wordPart={wordPart} onMastered={onMastered} onNeedsWork={onNeedsWork} />);
 
-        const needsWorkButton = screen.getByRole("button", {name: "Needs work" });
+        const needsWorkButton = screen.getAllByText("Needs work")[0];
         fireEvent.click(needsWorkButton);
 
         expect(onNeedsWork).toHaveBeenCalled();
@@ -22,12 +22,15 @@ describe("WordPartDetail", () => {
         const onNeedsWork = vi.fn();
         const onMastered = vi.fn();
 
-        const wordPart: WordPart = {id: 1, label: "test"};
+        const wordPart: WordPart = { id: 1, label: "test", phonics_level_id: 1 };
 
-        render(<WordPartDetail wordPart={wordPart} onMastered={onMastered} onNeedsWork={onNeedsWork}/>);
+        render(<WordPartDetail wordPart={wordPart} onMastered={onMastered} onNeedsWork={onNeedsWork} />);
 
-        const masteredButton = screen.getByRole("button", {name: "Needs work" });
-        fireEvent.click(masteredButton);
+        screen.getAllByRole("button").map((button) => {
+            if (button.textContent === "Got it!") {
+                return button.click();
+            }
+        });
 
         expect(onMastered).toHaveBeenCalled();
     });
