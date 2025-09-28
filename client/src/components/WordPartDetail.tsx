e80fcdfb99582a57d0871aa74565df95e0220790
niltron <51491500+niltron@users.noreply.github.com>
2025-09-28 17:20:29 +0000

    Add some  colors

--- client/src/components/WordPartDetail.tsx
+++ client/src/components/WordPartDetail.tsx
@@ -8,13 +8,13 @@ interface LargeWordPartProps {
 
 export default function WordPartDetail({ wordPart, onNeedsWork, onMastered }: LargeWordPartProps) {
   return (
-    <div className="w-full flex flex-col gap-6 justify-center items-center mt-8">
-      <span data-testid="largeWordPart" className="text-6xl">
+    <div className="w-full flex flex-col gap-6 justify-center items-center my-4">
+      <div data-testid="largeWordPart" className={`w-full text-center p-4 rounded text-6xl bg-gray-400 text-white ${wordPart.status === 'mastered' ? 'bg-green-400' : ''} ${wordPart.status === 'needs_work' ? 'bg-red-400' : ''}`}>
         {wordPart.label}
-      </span>
+      </div>
       <div className="flex flex-row w-full justify-between my-4">
-        <button type="button" onClick={onNeedsWork} className="w-[120px] px-4 py-2 bg-red-500 text-white rounded hover:bg-green-600">Needs work</button>
-        <button type="button" onClick={onMastered} className="w-[120px] px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Got it!</button>
+        <button type="button" onClick={onNeedsWork} className="w-[120px] px-4 py-2 bg-red-500 text-white rounded hover:bg-gray-600">Needs work</button>
+        <button type="button" onClick={onMastered} className="w-[120px] px-4 py-2 bg-green-500 text-white rounded hover:bg-gray-600">Got it!</button>
       </div>
     </div>
   )
